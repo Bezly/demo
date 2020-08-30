481fcfb1c95f5aa0e1a7054e258e6b9c63538b90
Dmitry Bezly <bezlydmitry@outlook.com>
2020-08-30 12:35:48 +0300

    3

--- develop.js
+++ develop.js
@@ -3,6 +3,10 @@ console.log('new feature was addded')
 
 console.log('new-feature commit second part')
 
+<<<<<<< Updated upstream
 
 
 console.log('new-feature4')
+=======
+console.log('new-feature3')
+>>>>>>> Stashed changes
