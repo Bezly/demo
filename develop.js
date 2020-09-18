8b072cbb29136d641e167ccbde1f24ecf1e7309f
Dmitry Bezly <bezlydmitry@outlook.com>
2020-09-18 18:18:52 +0300

    co

--- develop.js
+++ develop.js
@@ -5,6 +5,8 @@ console.log('new-feature commit second part')
 
 
 console.log('finally it is working for me. hell yeah')
+console.log('finally it is working for me. hell yeah')
+console.log('finally it is working for me. hell yeah')
 
 
 console.log('new-feature4')
